99dd86fc55dd04f958253f9d055a86dd256e40f1
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-05-14 22:43:37 -0300

    Implemente Inserção recursiva AVL

--- src/lib/ArvoreAVL.java
+++ src/lib/ArvoreAVL.java
@@ -9,5 +9,22 @@ public class ArvoreAVL <T> extends ArvoreBinaria<T>{
     }
 
     //Implementar métodos para efetuar o balanceamento e sobrescrever método de adicionar elemento...
+    @Override
+    protected No<T> inserirRecursivo(No<T> raiz, T novo){
+        raiz = super.inserirRecursivo(raiz,novo);
+
+        if(raiz.fatorBalanceamento()>1){
+            if(raiz.getFilhoDireita().fatorBalanceamento()>0)
+                raiz = this.rotacaoEsquerda(raiz);
+            else
+                raiz = this.rotacaoDireitaEsquerda(raiz);
+        }
+        else if(raiz.fatorBalanceamento()<-1){
+            if(raiz.getFilhoEsquerda().fatorBalanceamento()<0)
+                raiz = this.rotacaoDireita(raiz);
+            else raiz = this.rotacaoEsquerdaDireita(raiz);
+        }
+        return raiz;
+    }
 
 }
